0a9b63110a7b7ba2bd6c3b4bf6c9964c07a4f3c0
Vishesh Pandey <developer.vishesh@gmail.com>
2025-09-06 17:54:59 +0530

    added backend setup

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,12 @@ from fastapi import FastAPI, APIRouter
 from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
 import os
+from backend.routers.user import router as user_router
+from backend.routers.travel import router as travel_router
+from backend.routers.budget import router as budget_router
+from backend.routers.hotel import router as hotel_router
+from backend.routers.itinerary import router as itinerary_router
+from backend.database.database import Base, engine
 
 app = FastAPI()
 
@@ -11,5 +17,13 @@ backend_router = APIRouter(prefix="/backend")
 def ping():
     return {"message": "pong"}
 
+# Create tables
+Base.metadata.create_all(bind=engine)
+
 app.include_router(backend_router)
+app.include_router(user_router)
+app.include_router(travel_router)
+app.include_router(budget_router)
+app.include_router(hotel_router)
+app.include_router(itinerary_router)
 
